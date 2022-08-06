0e2ad08d187bda27848d70642f5414ee53743bab
andrew-lange13 <andrew.lange13@gmail.com>
2022-08-06 12:01:47 -0700

    starting palindrome check

---
+++ palindrome.cbl
@@ -0,0 +1,14 @@
+       identification division.
+       program-id. palindrome.
+
+       environment division.
+       
+
+       data division.
+       
+
+       procedure division.
+       
+
+       
+       
